b0d88545ff10290c9f4fea1dc8c08c5e03d75e20
U-GXP\ka.wada <ka.wada@DPC-1202.gxp.local>
2017-06-14 14:53:35 +0900

    first commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Todo> todolist;
+
+        public MainWindow()
+
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRunQuery_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new HttpClient();
+            var uri = "http://localhost:8080/api/todo";
+            var result = await client.GetStringAsync(uri);
+            todolist = JsonConvert.DeserializeObject<ObservableCollection<Todo>>(result);
+            this.todolistView.ItemsSource = todolist;
+        }
+    }
+}
---
+++ WpfApp1/Todo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Todo : INotifyPropertyChanged
+    {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                this.id = value;
+                RaisePropertyChanged("Id");
+            }
+        }
+
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                this.title = value;
+                RaisePropertyChanged("Title");
+            }
+        }
+        private string deadline;
+        public string Deadline
+        {
+            get { return deadline; }
+            set
+            {
+                this.deadline = value;
+                RaisePropertyChanged("Deadline");
+            }
+        }
+
+        public Todo(int id, string title, string deadline)
+        {
+            this.id = id;
+            this.title = title;
+            this.deadline = deadline;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
